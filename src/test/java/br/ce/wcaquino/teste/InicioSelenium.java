743e990cde46794fa2f5f3c86f3215bc803768b5
Aldrovando CAMARGO Neves <aldrov_camargo@hotmail.com>
2021-07-18 22:03:07 -0300

    Correcao de estrutura

---
+++ src/test/java/br/ce/wcaquino/teste/InicioSelenium.java
@@ -0,0 +1,12 @@
+package br.ce.wcaquino.teste;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class InicioSelenium {
+
+	public static void main(String[] args) {
+		//System.setProperty("webdriver.chrome.driver", "C:\\windrivers\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+	}
+
+}
