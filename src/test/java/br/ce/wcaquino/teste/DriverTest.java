11245f0d181017c59f6109c2f34b80376a9099f1
Aldrovando CAMARGO Neves <aldrov_camargo@hotmail.com>
2021-07-18 22:33:58 -0300

    Driver test

--- src/test/java/br/ce/wcaquino/teste/DriverTest.java
+++ src/test/java/br/ce/wcaquino/teste/DriverTest.java
@@ -2,10 +2,11 @@ package br.ce.wcaquino.teste;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class InicioSelenium {
+public class DriverTest {
 
 	public static void main(String[] args) {
-		//System.setProperty("webdriver.chrome.driver", "C:\\windrivers\\chromedriver.exe");
+		// TODO Auto-generated method stub
+		System.out.println("Main...");
 		ChromeDriver driver = new ChromeDriver();
 	}
 
