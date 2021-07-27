66e3d3b6fba6bc5ccc95e4eef4645ce8750d08f5
Aldrovando CAMARGO Neves <aldrov_camargo@hotmail.com>
2021-07-27 10:56:39 -0300

    Estrategia 3 basica

--- src/test/java/br/ce/wcaquino/estrategia3/GeradorMassas.java
+++ src/test/java/br/ce/wcaquino/estrategia3/GeradorMassas.java
@@ -8,9 +8,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import com.github.javafaker.Faker;
 
+import br.ce.wcaquino.entidades.Conta;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.service.ContaService;
+import br.ce.wcaquino.service.UsuarioService;
+
 public class GeradorMassas {
 	
 	public static final String CHAVE_CONTA_SB = "CONTA_SB";
+	public static final String CHAVE_CONTA = "CONTA";
 	
 	public void geradorContaSeuBarriga() throws ClassNotFoundException, SQLException {
 		ChromeDriver driver = new ChromeDriver();
@@ -29,15 +35,25 @@ public class GeradorMassas {
 		driver.quit();
 		
 		new MassaDAOImpl().inserirMassa(CHAVE_CONTA_SB, registro);
-		
 	}
 	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-//		GeradorMassas gerador = new GeradorMassas();
-//		for(int i =0; i <5; i++) {
-//			gerador.geradorContaSeuBarriga();
-//		}
-		String massa = new MassaDAOImpl().obterMassa(CHAVE_CONTA_SB);
-		System.out.println(massa);
+	public void geradorConta() throws Exception {
+		Faker faker = new Faker();
+		ContaService service = new ContaService();
+		UsuarioService userService = new UsuarioService();
+		Usuario usuarioGlobal = new Usuario(faker.name().fullName(), faker.internet().emailAddress(), faker.internet().password());
+		Usuario usuarioSalvo = userService.salvar(usuarioGlobal);
+		Conta conta = new Conta(faker.superhero().name(), usuarioSalvo);
+		service.salvar(conta);
+		new MassaDAOImpl().inserirMassa(CHAVE_CONTA, conta.getNome());
+	}
+	
+	public static void main(String[] args) throws Exception {
+		GeradorMassas gerador = new GeradorMassas();
+		for(int i =0; i <10; i++) {
+			gerador.geradorConta();
+		}
+//		String massa = new MassaDAOImpl().obterMassa(CHAVE_CONTA_SB);
+//		System.out.println(massa);
 	}
 }
